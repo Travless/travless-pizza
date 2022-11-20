82c37b7cb300fa75497f109c20d3c8f4ac1af186
Travless <travis.white0101@gmail.com>
2022-11-20 00:12:06 -0500

    add nav menu via DOM elements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const pageLoad = (() => {
+const pageLoad = () => {
     // content container
     const content = document.getElementById('content');
 
@@ -11,7 +11,40 @@ const pageLoad = (() => {
     logoContainer.setAttribute('id', 'logo-container')
     nav.append(logoContainer);
     const logo = document.createElement('img');
+    logo.setAttribute('id', 'logo')
     logo.src = './images/travless-logo.svg';
     logoContainer.append(logo);
+
+    const praiseConatiner = document.createElement('div');
+    praiseConatiner.setAttribute('id', 'praise-container');
+    nav.append(praiseConatiner);
+    const praise = document.createElement('div');
+    praise.setAttribute('id', 'praise');
+    praise.textContent('Rated Best Pizza in the Burgh for 2022!');
+    praiseConatiner.append(praise);
+
+    const navMenu = document.createElement('div');
+    navMenu.setAttribute('id', 'nav-menu');
+    nav.append('navMenu');
+    const menu = document.createElement('a');
+    menu.setAttribute('id', 'menu');
+    menu.textContent = 'Menu';
+    navMenu.append(menu);
+    const order = document.createElement('a');
+    order.setAttribute('id', 'order');
+    order.textContent = 'Order';
+    navMenu.append(order);
+    const aboutUs = document.createElement('a');
+    aboutUs.setAttribute('id', 'about-us');
+    aboutUs.textContent = 'About Us';
+    navMenu.append(aboutUs);
+    const contact = document.createElement('a');
+    contact.setAttribute('id', 'contact');
+    contact.textContent = 'Contact';
+    navMenu.append(contact);
+
+    // body container
     
-})();
+};
+
+pageLoad();
